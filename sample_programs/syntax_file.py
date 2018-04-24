19c65ab03b2c165e756871690af80cc716e90585
Bhagyaraj Chitoth <Bhagyarajc@gmail.com>
2018-04-24 16:20:13 +0530

    updates

---
+++ sample_programs/syntax_file.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 16 20:36:10 2018
+
+@author: bhagu
+"""
+
+
+def main():
+    print('This is the Main Section')
+    
+if __name__ == '__main__':
+    main()
